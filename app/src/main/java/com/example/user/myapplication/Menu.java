4c260f8f350e5ca1d00928369c5b97b0addbba89
PNakraszewicz <pawel.nakraszewicz@gmail.com>
2017-04-24 10:34:52 +0200

    dodanie funkcji gps

---
+++ app/src/main/java/com/example/user/myapplication/Menu.java
@@ -0,0 +1,49 @@
+package com.example.user.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+
+public class Menu extends Activity
+{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu1);
+
+        getWindow().getDecorView().setBackgroundColor(Color.parseColor("#25315e"));
+
+        ImageButton compass = (ImageButton) findViewById(R.id.item1);
+        compass.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                Intent intention = new Intent(getApplicationContext(), Compass.class);
+                startActivity(intention);
+            }
+        });
+
+        ImageButton gpsButton = (ImageButton) findViewById(R.id.item3);
+        gpsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                Intent intention = new Intent(getApplicationContext(), AndroidGPSTrackingActivity.class);
+                startActivity(intention);
+            }
+
+
+        });
+
+    }
+};
